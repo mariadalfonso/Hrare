e455d8929df893478723b5397780a5c0bc7e9d67
maria <dalfonso@cern.ch>
2021-10-06 15:45:14 +0200

    add files with some utility

---
+++ analysis/utilsHrare.py
@@ -0,0 +1,16 @@
+import ROOT
+import os
+from subprocess import call,check_output
+
+def findDataset(name):
+
+    DASclient = "dasgoclient -query '%(query)s'"
+    cmd= DASclient%{'query':'file dataset=%s'%name}
+    print(cmd)
+    check_output(cmd,shell=True)
+    fileList=[ 'root://xrootd-cms.infn.it//'+x for x in check_output(cmd,shell=True).split() ]
+
+    files_ROOT = ROOT.vector('string')()
+    for f in fileList: files_ROOT.push_back(f)
+
+    return files_ROOT
