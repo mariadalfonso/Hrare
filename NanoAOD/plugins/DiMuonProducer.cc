e4d130385d9d2c30cae8f1afcf02dc8231e4cd08
maria <dalfonso@cern.ch>
2024-10-02 19:38:38 +0200

    update for Run3

--- NanoAOD/plugins/DiMuonProducer.cc
+++ NanoAOD/plugins/DiMuonProducer.cc
@@ -2,7 +2,7 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/Utilities/interface/StreamID.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/EventSetup.h"
@@ -220,7 +220,7 @@ using namespace std;
 ///                             P L U G I N
 ///////////////////////////////////////////////////////////////////////////
 
-class DiMuonProducer : public edm::EDProducer {
+class DiMuonProducer : public edm::stream::EDProducer<> {
 
 public:
     
@@ -360,9 +360,9 @@ private:
 };
 
 DiMuonProducer::DiMuonProducer(const edm::ParameterSet &iConfig):
-theTTBuilderToken_(esConsumes<TransientTrackBuilder, TransientTrackRecord>(edm::ESInputTag("", "TransientTrackBuilder"))),
+theTTBuilderToken_(esConsumes<>(edm::ESInputTag("", "TransientTrackBuilder"))),
 theTTBuilder_(nullptr),
-idealMagneticFieldRecordToken_(esConsumes<MagneticField, IdealMagneticFieldRecord>()),
+idealMagneticFieldRecordToken_(esConsumes<>()),
 magField_(nullptr),
 beamSpotToken_( consumes<reco::BeamSpot> ( iConfig.getParameter<edm::InputTag>( "beamSpot" ) ) ),
 beamSpot_(nullptr),
